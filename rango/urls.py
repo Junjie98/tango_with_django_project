d4a44e02a3d4d3e17e0f66bb46c9c44fe096b990
JunJie <nelsonlow_88@hotmail.com>
2021-07-27 00:24:00 +0100

    Updated the allowedhost, finished task 3.5

---
+++ rango/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+#the first parameter is the string to match in path.
+#the second parameter tells Django what view to call if the pattern '' is matched.
+#third parameter is optional called name. Convinient way to reference the view. & by naming our URL mapping
+#we can employ reverse URL matching. This is we can reference the URL mapping by name rather than by the URL.
+urlpatterns = [ 
+    path('', views.index, name='index'),
+]
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pyng$#)1!@*(v)nnq7#54u1nb$h#0_5m=(!j#ig&5aj$gll%7z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.56.1']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','192.168.56.1','100.81.197.12']
 
 
 # Application definition
