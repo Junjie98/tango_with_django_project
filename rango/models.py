c01b13c1cd10cd7110ff8738dd58f680e74c3b17
JunJie <nelsonlow_88@hotmail.com>
2021-07-29 04:49:51 +0100

    Finished Chapter5. Test passed

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
     #unique means the given field's value must be unique thruout the underlying database table.
 
     class Meta:
